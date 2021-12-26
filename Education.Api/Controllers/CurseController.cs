572060f06103bd995b36e94ff272b69a73c58a5b
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-26 13:16:41 -0300

    add api endpoints

---
+++ Education.Api/Controllers/CurseController.cs
@@ -0,0 +1,35 @@
+﻿using Education.Application.Curses;
+using Education.Application.DTO;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurseController : ControllerBase
+    {
+        private IMediator _mediator;
+        public CurseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CurseDTO>>> Get()
+        {
+            return await _mediator.Send(new GetCurseQuery.GetCurseQueryRequest());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Post(CreateCurseCommand.CreateCurseCommandRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+    }
+}
--- Education.Api/Program.cs
+++ Education.Api/Program.cs
@@ -1,13 +1,38 @@
+using Education.Application.Curses;
+using Education.Persistence;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<EducationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddMediatR(typeof(GetCurseQuery.GetCurseQueryHandler).Assembly);
+
+
+builder.Services.AddControllers()
+    .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<CreateCurseCommand>());
+
+builder.Services.AddAutoMapper(typeof(GetCurseQuery.GetCurseQueryHandler));
+
+builder.Services.AddCors(o => o.AddPolicy("corsApp", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("corsApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
